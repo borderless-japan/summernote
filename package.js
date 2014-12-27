870e94e63d06ab548a09beb93439afa134ccd799
Nicolás López <nicolaslopezj@me.com>
2014-12-27 03:27:48 -0300

    bug fix

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: 'orionjs:summernote',
 	summary: 'Summernote editor for orionjs',
-	version: '0.2.0',
+	version: '0.2.1',
 	git: 'https://github.com/orionjs/summernote'
 });
 
