df9209827d8f092837fff24a266e4fbad6cf6a1a
Nicolás López <nicolaslopezj@me.com>
2015-01-11 07:13:03 -0300

    Fixed #3

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: 'orionjs:summernote',
 	summary: 'Summernote editor for orionjs',
-	version: '0.2.3',
+	version: '0.2.4',
 	git: 'https://github.com/orionjs/summernote'
 });
 
@@ -9,11 +9,11 @@ Package.onUse(function(api) {
 	api.versionsFrom('1.0');
 
 	api.use([
-		'orionjs:core@0.3.0',
+		'orionjs:core@0.4.1',
 		'meteor-platform',
 		'less',
-		'aldeed:autoform@4.0.2', 
-		'orionjs:filesystem@0.0.12', 
+		'aldeed:autoform@4.2.2', 
+		'orionjs:filesystem@0.0.13', 
 		'summernote:standalone@0.6.0',
 		'jquery@1.0.0'
 		]);
